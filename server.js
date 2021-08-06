47d180c7b40b711cd6b98b2c8ee5d85179fb9da8
dharmendra <devdharmendra81@gmail.com>
2021-08-06 21:38:52 +0530

    changed to async code

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const AggregateService = require('./service/AggregateService');
 
 const url = "mongodb://localhost:27017/";
 
-MongoClient.connect(url, function(err, mongoDB) {
+MongoClient.connect(url, async (err, mongoDB) => {
   if (err) {
     throw err;
   }
   const db = mongoDB.db("testDB");
-  AggregateService.aggregateHistory(db);
+  AggregateService.init(db);
+  await AggregateService.aggregateHistory();
 });
